7218b5367d5595df1f7b1427153c81a26a380376
Tom Barber <tom@analytical-labs.com>
2011-10-10 17:10:21 +0100

    server user

---
+++ manifests/definitions/sshserveruser.pp
@@ -0,0 +1,5 @@
+define sshserveruser ($ensure = "present") {
+  realize File["/home/${title}/.ssh"]
+  User <| title == $title |> { ensure => $ensure }
+  ssh::auth::server { "${title}@analytical-labs.com": ensure => $ensure }
+}
