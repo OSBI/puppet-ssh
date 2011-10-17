cecbc0a08508b3f68d045b7a7bd35c3a6874f3ed
Tom Barber <tom@analytical-labs.com>
2011-10-17 14:53:00 +0100

    ssh

--- manifests/definitions/sshclient.pp
+++ manifests/definitions/sshclient.pp
@@ -1,4 +1,5 @@
 define sshclient (){
+  File["/home/${title}"]
   realize File["/home/${title}/.ssh"]
   User <| title == $title |> { ensure => $ensure }
   ssh::auth::client { "${title}@analytical-labs.com": }
