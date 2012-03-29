40376ef63a0914adb1c40fa0fc3fd302ed4e8b7b
Tom Barber <tom@analytical-labs.com>
2012-03-29 13:22:17 +0100

    new files

---
+++ manifests/sftpserver.pp
@@ -0,0 +1,30 @@
+class ssh::sftpserver{
+	
+	file{ 
+		"/etc/ssh2":
+		ensure => directory,
+	}
+	
+	file {
+		"/etc/ssh2/sshd_config":
+		ensure => present,
+		source => "puppet:///modules/ssh/sftp/sshd_config",
+		require => File["/etc/ssh2"],
+	}
+	
+	file {
+		"/etc/default/ssh2":
+		ensure => present,
+		source => "puppet:///modules/ssh/sftp/ssh2",
+				require => File["/etc/ssh2"],
+		
+	}
+	
+	file {
+		"/etc/init.d/ssh2":
+		ensure => present,
+		source => "puppet:///modules/ssh/sftp/sshd2_init",
+				require => File["/etc/ssh2"],
+		
+	}
+}
